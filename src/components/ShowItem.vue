5b7dd304a4646d68563f855a80ffd18d3aa5d4eb
AliceCaselli <alicecaselli3@gmail.com>
2023-04-12 16:33:11 +0200

    Upsection draft complete

--- src/components/ShowItem.vue
+++ src/components/ShowItem.vue
@@ -5,22 +5,104 @@ export default {
     name: 'ShowItem',
     data() {
         return {
-            items: [
-                {
 
-                }
-            ]
         };
     },
+
+    props: {
+        item: Object,
+
+    }
 };
 
 </script>
 
 <template>
     <div class="item">
-        <button></button>
-        <div class="text"></div>
+
+        <img :src="item.image" alt="">
+        <div class="btn-item">
+
+            <button v-for="button in item.buttons">{{ button }}</button>
+        </div>
+        <div class="text">
+
+            <div class="first-row">
+
+                <span class="user"><i class="fa-solid fa-user"></i>{{ item.user }}</span>
+                <span class="date"><i class="fa-solid fa-calendar-days"></i>{{ item.date }}</span>
+            </div>
+            <div class="title">{{ item.title }}</div>
+
+        </div>
     </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.item {
+    position: relative;
+    height: calc(100% / 2 - 5px);
+    width: calc(100% / 4);
+
+
+    &:nth-child(3) {
+        height: 100%;
+        width: calc(100% / 2);
+    }
+
+    img {
+        height: 100%;
+    }
+
+    .btn-item {
+        position: absolute;
+        display: flex;
+        flex-direction: row;
+        gap: 10px;
+        left: 50%;
+        transform: translateX(-50%);
+        top: 20px;
+
+        button {
+            background-color: #fff;
+            border: transparent;
+            color: #333333;
+            padding: 5px;
+        }
+    }
+
+    .text {
+        position: absolute;
+        width: 100%;
+
+
+        z-index: 2;
+        bottom: 25px;
+        color: white;
+
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+
+        .first-row {
+            width: 100%;
+            display: flex;
+            flex-direction: row;
+            justify-content: center;
+            gap: 10px;
+
+            .user,
+            .date {
+                display: flex;
+                flex-direction: row;
+                gap: 5px;
+                align-items: baseline;
+            }
+        }
+
+        .title {
+            font-weight: bold;
+        }
+    }
+}
+</style>
--- src/components/ShowtableApp.vue
+++ src/components/ShowtableApp.vue
@@ -6,7 +6,61 @@ export default {
 
     name: 'ShowtableApp',
     data() {
-        return {};
+        return {
+            items: [
+                {
+                    image: 'img/anime-fashion.webp',
+                    buttons: [
+                        'Fashion',
+                        'Lifestyle',
+                    ],
+                    user: 'demo',
+                    date: 'December 25, 2022',
+                    title: 'Fashion Trend Now A Days',
+                },
+                {
+                    image: 'img/travel-alone.webp',
+                    buttons: [
+                        'Stories',
+                        'Travel',
+                    ],
+                    user: 'demo',
+                    date: 'December 26, 2022',
+                    title: 'Traveling Alone Is Awesome',
+                },
+                {
+                    image: 'img/best-places.webp',
+                    buttons: [
+                        'Lifestyle',
+                        'Stories',
+                        'Travel',
+                    ],
+                    user: 'demo',
+                    date: 'December 25, 2022',
+                    title: 'Places For A Road Trip',
+                },
+                {
+                    image: 'img/music-love.webp',
+                    buttons: [
+                        'Culture',
+                        'Lifestyle',
+                    ],
+                    user: 'demo',
+                    date: 'December 25, 2022',
+                    title: 'Music The Love Of My Life',
+                },
+                {
+                    image: 'img/visit-france.webp',
+                    buttons: [
+                        'Lifestyle',
+                        'Travel',
+                    ],
+                    user: 'demo',
+                    date: 'December 26, 2022',
+                    title: 'Reasons To Visit France',
+                }
+            ]
+        };
     },
 
     components: {
@@ -17,7 +71,20 @@ export default {
 </script>
 
 <template>
-    <ShowItem></ShowItem>
+    <div class="table">
+        <ShowItem v-for="item in items" :item="item"></ShowItem>
+    </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.table {
+    max-width: 1200px;
+    margin: 0 auto;
+    height: 450px;
+    display: flex;
+    flex-flow: column wrap;
+    gap: 10px;
+    justify-content: space-between;
+
+}
+</style>
